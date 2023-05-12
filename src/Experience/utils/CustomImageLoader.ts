e2026ed11573d4485d251a8e279bbbc61f9828df
romaindasilva <romain.dasilva@lachouette.co>
2023-05-12 11:04:04 +0200

    🚧 wip

--- src/Experience/utils/CustomImageLoader.ts
+++ src/Experience/utils/CustomImageLoader.ts
@@ -1,11 +1,13 @@
-import { ImageLoader, Texture } from "three";
+import { ImageLoader, Texture, TextureLoader } from "three";
 
 export default class CustomImageLoader {
   private static instance: CustomImageLoader;
   private static imageLoader: ImageLoader;
+  private static textureLoader: TextureLoader;
   private constructor() {
     CustomImageLoader.instance = this;
     CustomImageLoader.imageLoader = new ImageLoader();
+    CustomImageLoader.textureLoader = new TextureLoader();
   }
 
   public static getInstance(): CustomImageLoader {
@@ -17,5 +19,9 @@ export default class CustomImageLoader {
 
   loadOneImageAsTexture(url: string): Texture {
     return new Texture(CustomImageLoader.imageLoader.load(url));
+  }
+
+  loadImage(url: string) {
+    return CustomImageLoader.textureLoader.load(url);
   }
 }
